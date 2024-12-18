c0b3f19344ceba18e58816e6f2cd54398d4a0885
Amir Azad <amir.azad78@gmail.com>
2024-12-18 11:32:07 +0330

    add limiter version

--- src/api/middlewares/limiter.go
+++ src/api/middlewares/limiter.go
@@ -1,18 +1,13 @@
 package middlewares
 
 import (
-	"github.com/didip/tollbooth/v8"
-	"github.com/didip/tollbooth/v8/limiter"
+	"github.com/didip/tollbooth"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func LimitByRequest() gin.HandlerFunc {
 	lmt := tollbooth.NewLimiter(1, nil)
-	lmt.SetIPLookup(limiter.IPLookup{
-		Name:           "X-Forwarded-For",
-		IndexFromRight: 0,
-	})
 	return func(c *gin.Context) {
 		err := tollbooth.LimitByRequest(lmt, c.Writer, c.Request)
 		if err != nil {
